14d9c50893c7f77ed56aaeb09b2fe2101fdd6605
Garland Xie <garlandxie@gmail.com>
2021-09-15 20:47:05 -0400

    Moved calculation of guo's index to another R script

--- src/01-data_clean_biomass.R
+++ src/01-data_clean_biomass.R
@@ -58,19 +58,3 @@ bm_tidy %>%
   coord_flip() + 
   theme_bw()
 
-# Guo's invasibility
-
-sr_max <- max(bm_tidy$sr)
-bm_max <- max(bm_tidy$bm_g)
-
-bm_tidy$bm_obs_max <- bm_tidy$bm_g/bm_max
-bm_tidy$sr_obs_max <- bm_tidy$sr/sr_max
-
-bm_tidy %>%
-ggplot(aes(x = sr_obs_max, y = bm_obs_max)) +
-  geom_point() +
-  labs(
-    x = expression("S"["obs"]/"S"["max"]),
-    y = expression("B"["obs"]/"B"["max"])
-    ) + 
-  theme_bw()
