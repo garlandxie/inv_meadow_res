18933e123dff80224b471f96bfbeab7126d646f1
Garland Xie <garlandxie@gmail.com>
2022-04-26 16:52:42 -0400

    Initial commit

---
+++ src/processing_scripts/03-process_seed_bank_data.R
@@ -0,0 +1,58 @@
+# libraries ----
+library(here)
+library(dplyr)
+library(janitor)
+
+# import ----
+sb_spring <- read.csv(
+  here(
+    "data", 
+    "input_data", 
+    "seed_bank", 
+    "seed_bank_spring.csv")
+  )
+
+sb_fall <- read.csv(
+  here(
+    "data", 
+    "input_data", 
+    "seed_bank",
+    "seed_bank_fall.csv")
+)
+
+# check packaging ----
+dplyr::glimpse(sb_spring)
+
+# clean data ----
+
+## combine both spring and fall sampling seasons ----
+sb_spr_fall <- sb_fall %>%
+  select(-Typed_by) %>%
+  rbind(sb_spring)
+
+## calculate seed bank density ----
+sb_spring_tidy <- sb_spr_fall %>%
+  janitor::clean_names() %>%
+  dplyr::filter(
+    site_name %in% c("BNSH", "GRNB", "DAVE", "KENN", "TIMH", "VICP")
+    ) %>%
+  group_by(section, site_name, treatment, plot) %>%
+  summarize(
+    sb_density = sum(abund, na.rm = TRUE), 
+    sb_richness = dplyr::n_distinct(spp_code)) %>%
+  ungroup()
+
+## calculate propagule pressure of V. rossicum ----
+
+# no seeds of V.rossicum emerged from the greenhouse soil seed banks
+sb_spring_tidy$sb_pp_viro <- 0
+
+# save to disk ----
+
+write.csv(
+  x = sb_spring_tidy, 
+  file = here("data", "intermediate_data", "seed_bank_tidy.csv")
+)
+
+
+
