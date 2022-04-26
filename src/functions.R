7292b72c042c5a074bea3c59638fd501159a1a6a
Garland Xie <garlandxie@gmail.com>
2022-04-26 17:04:44 -0400

    Change custom functions

--- src/functions.R
+++ src/functions.R
@@ -1,38 +1,32 @@
-#' Convert the coordinates from a tibble of character types into a vector of 
-#' numeric types
-#' 
-#' @param data_to_convert the tibble that you want to convert
-#' @param col_number the column number of the column you want to convert
-#' @return The column as numeric types in a vector
-#' @examples 
-#' A vector containing the degrees for coords
-#' as.numeric(unlist(c(coords_lat3[2])))
-#' [1] 43 43 43 43 43 43 43 43 43 43 43 43 43 43 43 43 43 43 43 43 43 43 43 43
-#' [25] 43 43 43 43 43 43 43 43 43 43 43 43
+# TO DO: add roxygen documentation
 
-dms_col <- function(data_to_convert, col_number) {
-  dms <- as.numeric(unlist(c(data_to_convert[col_number])))
-  return (dms)
+import_gpx <- function(link) {
+  link2 <- here("data", "input_data", "seed_rain", link)
+  sf_seed_rain <- sf::st_read(link2, layer = "waypoints")
+  return(sf_seed_rain)
 }
 
-#' Converts DMS to decimal degrees
-#' 
-#' @param degrees degrees as a double, if NA it is taken as 0
-#' @param minutes minutes as a double, if NA it is taken as 0
-#' @param seconds seconds as a double, if NA it is taken as 0
-#' @return the degrees, minutes, and seconds converted to decimal degrees as a 
-#' double
-#' @examples 
-#' dms_to_dd(75, 32, 21)
-#' 74.53917
-#' dms_to_dd(34, 72, NA)
-#' 35.2
-
-dms_to_dd <- function(degrees, minutes, seconds) {
-  if(is.na(degrees) == TRUE) {degrees = 0} else{degrees}
-  if(is.na(minutes) == TRUE) {minutes = 0} else{minutes}
-  if(is.na(seconds) == TRUE) {seconds = 0} else{seconds}
-  converted <- degrees + (minutes/60) + (seconds/3600)
-  return(converted)
+calc_seed_rain_index<- function(plot, seed_rain, distance = 10) {
+  
+  # create buffer
+  buffer <- st_buffer(plot, dist = distance)
+  
+  # clip seed rain to a specific buffer
+  clip_sr <- st_intersection(seed_rain, buffer)
+  
+  # calculate matrix of distances from focal point to seed rain
+  distance_sr <- st_distance(plot, clip_sr)
+  distance_sr <- as.numeric(distance_sr)
+  
+  # calculate seed rain index
+  dispersal_coef <- 1.49 # arbitrary number
+  fecundity <- 100 # arbitrary number
+  seed_rain <- fecundity*exp((-dispersal_coef)*distance_sr)
+  seed_rain_index <- sum(seed_rain)
+  
+  return(seed_rain_index)
+  
 }
+
+
 
