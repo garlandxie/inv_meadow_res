e82a4d8eea5f97544472703920aded8a97dca233
Garland Xie <garlandxie@gmail.com>
2022-04-27 15:28:54 -0400

    Add comments for R libraries

--- src/processing_scripts/02-calculate_di.R
+++ src/processing_scripts/02-calculate_di.R
@@ -1,10 +1,10 @@
 # libraries ----
-library(here)
-library(rdryad)
-library(dplyr)
-library(janitor)
-library(stringr)
-library(ggplot2)
+library(here)    # for creating relative file paths
+library(rdryad)  # for importing dryad repos
+library(dplyr)   # for manipulating data
+library(janitor) # for cleaning column names
+library(stringr) # for manipulating string data
+library(ggplot2) # for visualising data 
 
 # import ----
 
