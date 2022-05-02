1607261660b08e0a139ad035a90828dfb5c83cd4
Garland Xie <garlandxie@gmail.com>
2022-05-02 16:35:43 -0400

    Intial commit

---
+++ src/processing_scripts/05_01_calc-dispersal_coefs.R
@@ -0,0 +1,63 @@
+# libraries ----
+library(dplyr)    # for manipulating data
+library(here)     # for creating relative file-paths
+library(ggplot2)  # for visualizing data 
+library(MESS)     # for calculating area under curves
+
+# import ----
+
+# can't download zip files from the R DRYAD API
+# so, manual download and import into R
+# doi: 10.5061/dryad.mq2ff
+
+disp_df <- read.csv(
+  here("data", "input_data", "seed_rain", "Plantdispersaldata.csv")
+  )
+
+# clean data ---
+
+# doi for this specific dispersal kernel dataset
+# https://doi.org/10.1674/0003-0031(2002)148[0263:SOTIAV]2.0.CO;2
+dsv_1 <- row.names(disp_df)[disp_df$Species.name == "Vincetoxicum rossicum"]
+dsv_1 <- as.numeric(dsv_1)
+dsv_2 = dsv_1 + 1
+
+disp_tidy <- disp_df %>%
+  slice(dsv_1:dsv_2) %>%
+  select(-"Dataset", -"Species.name", -"Source") %>%
+  t() %>%
+  as.data.frame() %>%
+  dplyr::filter(V1 != "Density (m-2)") %>%
+  na.omit() %>%
+  mutate(
+    V1 = stringr::str_replace(V1, pattern = " ", replace = ""), 
+    V1 = as.numeric(V1), 
+    V2 = as.numeric(V2)
+  ) %>%
+  rename(distance_m = V1, density_m2 = V2) 
+
+# plot ----
+
+# dispersal location kernel
+  disp_tidy %>%
+    ggplot(aes(x = distance_m, y = density_m2)) + 
+    geom_point() + 
+    geom_line() + 
+    labs(
+      title = "Dispersal location kernel for Vincetoxicum rossicum",
+      x = "Distance (m)", 
+      y = "Density (m^2)") + 
+    theme_bw()
+
+# get dispersal cofficients -----
+
+# calculate area under the curve using definite integrals
+# natural spline interpolation
+# from a vector of x and y coordinates
+
+# includes this parameter 
+# in calc_seed_rain_index() in the functions.R file 
+disperal_coef <- MESS::auc(
+  x = disp_tidy$distance_m, 
+  y = disp_tidy$density_m2, 
+  type = "spline")
