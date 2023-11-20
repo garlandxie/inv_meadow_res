ad9686ab8eb293297224011bf8fe02ee2dca4cf4
Garland Xie <garlandxie@gmail.com>
2023-11-20 15:36:10 -0500

    Changes specifications for one model + added more EDA graphs

--- src/analysis_scripts/01_sem.R
+++ src/analysis_scripts/01_sem.R
@@ -4,7 +4,8 @@ library(piecewiseSEM)  # for doing piece-wise structural equation modelling
 library(dplyr)         # for manipulating data
 library(lme4)          # for doing mixed effects models 
 library(DHARMa)        # for running diagnostic tests on mixed effects models
-library(partR2)        # for calculating part R2 for an individual predictor
+library(lmerTest)      # for getting p-values for the mixed effect models
+library(ggplot2)       # for visualizing datasets
 
 # import -----------------------------------------------------------------------
 sem_df <- read.csv(
@@ -51,9 +52,54 @@ sb_inv_lm <- glmer(
 ## |- invasibility -> degree of invasion ---------------------------------------
 
 # fit model
-sb_inv_lm <- glmer(
-  d ~ scale(sb_density) + (1|site), 
-  family = binomial(link = "log"),
+inv_di_lm <- lmer(
+  car::logit(guo_di_exo) ~ i_e_chal + treatment + (1|site), 
   data = sem_tidy
 )
+
+# plot -------------------------------------------------------------------------
+
+## |- TRCA meeting -------------------------------------------------------------
+di_vs_inv_by_trt <- sem_tidy %>%
+  filter(!(sb_density == max(sb_density))) %>% 
+  ggplot(aes(x = i_e_chal, y = guo_di_exo, fill = factor(treatment))) + 
+  geom_point() + 
+  geom_smooth(method = "lm", color = "black") + 
+  scale_fill_discrete(
+    name = "Management Regime", 
+    labels = c("Section 4 (Restored)", "Section 2 (Tilled)")
+  ) + 
+  xlim(0, 1) + 
+  ylim(0, 1) + 
+  labs(
+    x = "Susceptibility to Plant Invasion",
+    y = "Extent of Invasion") + 
+  theme_bw()
+
+sem_tidy %>%
+  filter(!(sb_density == max(sb_density))) %>% 
+  ggplot(aes(x = sb_density, y = i_e_chal, fill = factor(treatment))) + 
+  geom_point(aes(color = factor(treatment))) + 
+  geom_smooth(method = "lm", color = "black") + 
+  scale_fill_discrete(
+    name = "Management Regime", 
+    labels = c("Section 4 (Restored)", "Section 2 (Tilled)")  
+  ) +
+  theme_bw()
+
+# save to disk -----------------------------------------------------------------
+
+ggsave(
+  plot = di_vs_inv_by_trt,
+  file = here("output", "results", "di_vs_inv.pdf"),
+  device = "pdf", 
+  units = "in", 
+  height = 5, 
+  width = 7
+)
+
+
+
+
+
 
